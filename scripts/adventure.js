bb42c3af1428cbf1df1e8da545fdb65025414417
Paul Morales <paul@ubersmake.com>
2016-03-04 14:19:56 -0800

    Selectable (font) styles and themes.
    Selectable (font) styles and themes. And footer tweaks.

--- scripts/adventure.js
+++ scripts/adventure.js
@@ -4,8 +4,20 @@
 
 var title;
 var sections;
+var style = "sans";
+var theme = "light";
 
 $(document).ready(function() {
+    $("#style").change(function() {
+        style = $("#style option:selected").val();
+        changeStyle();
+    });
+
+    $("#theme").change(function() {
+        theme = $("#theme option:selected").val();
+        changeTheme();
+    });
+
     var storyPath = "./stories/";
     var story = "";
 
@@ -48,6 +60,9 @@ function displaySection(name) {
     }
 
     $("#content").html(html);
+
+    changeStyle();
+    changeTheme();
 }
 
 function getSection(name) {
@@ -101,6 +116,36 @@ function parseStory(data) {
     branches.push(bundle);
 
     return branches;
+}
+
+function changeStyle() {
+    if (style === "sans") {
+        $("#content").css("font-family", "sans-serif");
+    } else if (style === "serif") {
+        $("#content").css("font-family", "serif");
+    } else if (style === "mono") {
+        $("#content").css("font-family", "monospace");
+    }
+}
+
+function changeTheme() {
+    if (theme === "light") {
+        $("body").css("background-color", "#EEEEEE");
+
+        $("#content").css("color", "#424242");
+        $("#content a").css("color", "#212121");
+
+        $("#footer").css("background-color", "#9E9E9E");
+
+
+    } else if (theme === "dark") {
+        $("body").css("background-color", "#212121");
+
+        $("#content").css("color", "#BDBDBD");
+        $("#content a").css("color", "#FAFAFA");
+
+        $("#footer").css("background-color", "#9E9E9E");
+    }
 }
 
 function getURLParam(param) {
