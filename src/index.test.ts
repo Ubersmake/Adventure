0e447b4afc6ac17085f68308eb238e5ca2498876
Paul Morales <paul@ubersmake.com>
2024-05-06 15:57:01 -0700

    Use markdown-it (#6)
    * Try markdown-it.
    
    * Install @types/markdown-it.
    
    * Use Dinner for Two for testing.
    
    * Try parsing the file.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,23 @@
-import { hello } from './index';
+import { hello, parseFile } from './index';
+
+const source = './src/dinnerfortwo.md';
 
 test('Hello, world!', () => {
   expect(hello('world')).toBe('Hello, world!');
+});
+
+describe('Parse File', () => {
+  test('File exists', () => {
+    const output = parseFile(source);
+
+    expect(output).toBeTruthy();
+  });
+
+  test('File does not exist', () => {
+    const no = () => {
+      parseFile('no');
+    };
+
+    expect(no).toThrow();
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+import { readFileSync } from 'fs';
+import * as MarkdownIt from 'markdown-it';
+
 export function hello(subject: string): string {
   return `Hello, ${subject}!`;
+}
+
+export function parseFile(file: string): MarkdownIt.Token[] {
+  const md = MarkdownIt();
+
+  const content = readFileSync(file).toString();
+
+  const tokens = md.parse(content, {});
+
+  tokens.forEach((token) => {
+    console.log(token);
+  });
+
+  return tokens;
 }
